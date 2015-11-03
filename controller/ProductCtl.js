de10a19ea22f23637cc84617735daf5fd30d3ea0
LaurelSun <sainsun@qq.com>
2015-11-03 18:09:18 +0800

    add pager

--- controller/ProductCtl.js
+++ controller/ProductCtl.js
@@ -85,24 +85,42 @@ ProductCtl.prototype.list=function(req,res,next){
     //存在pageValue 使用gt 进行查询
     var filter=renderPage=="0"?{"$lt":new Date(createDate)}:{"$gt":new Date(createDate)};
     var queryOpt={softField:{inputDate:1},pageSize:conf.pager_querySize,
-        filter:'_id productName productCode productPutInDate productPutOutDate inputDate'};
-    ProdcutService.Query({'inputDate':filter},queryOpt,function(err,data){
+        filter:'_id productName productCode productPutInDate productPutOutDate inputDate status'};
+
+    var queryObj={};
+    queryObj['$and']=[];
+    queryObj['$and'].push({'inputDate':filter});
+
+    if(queryKey){
+        var searchObj={};
+        searchObj['$or']=[];
+        searchObj['$or'].push({productName:{$regex:queryKey}});
+        searchObj['$or'].push({productCode:{$regex:queryKey}});
+
+        queryObj['$and'].push(searchObj);
+
+    }
+
+    ProdcutService.Query(queryObj,queryOpt,function(err,data){
         if(err){
             console.log(err);
             next();
             return;
         }
 
+        var pageCnt=data.length<conf.page_cnt?data.length:conf.page_cnt;
+
         var productArr=[];
-        for(var i=0;i<conf.page_cnt;i++) {
-            data[i].productPutInDate=moment(data[i].productPutInDate).format('YYYY-MM-DD HH:mm:ss');
-            data[i].productPutOutDate=moment(data[i].productPutOutDate).format('YYYY-MM-DD HH:mm:ss');
+        for(var i=0;i<pageCnt;i++) {
+
+            data[i].putIn=data[i].productPutInDate?moment(data[i].productPutInDate).format('YYYY-MM-DD HH:mm:ss'):"N/A";
+            data[i].putOut=data[i].productPutOutDate?moment(data[i].productPutOutDate).format('YYYY-MM-DD HH:mm:ss'):"N/A";
 
             productArr.push(data[i]);
         }
         var pager=res.renderPager(currentPage,conf.pager_renderCnt,conf.page_cnt,'inputDate',data);
 
-        res.render('admin/product.html',{pager:pager,arr:productArr});
+        res.render('admin/product.html',{pager:pager,arr:productArr,cur:currentPage});
         //res.result(true,'',);
     });
 };
